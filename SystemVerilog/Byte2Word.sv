813c8cd5349e0d429326832d7bdef90164e372d0
justin killam <killamjustin@gmail.com>
2022-03-06 16:40:30 -0800

    Got the byte to word functionin

--- SystemVerilog/Byte2Word.sv
+++ SystemVerilog/Byte2Word.sv
@@ -18,22 +18,28 @@ module Byte2Word #(parameter BPW=4)
 (
 input clk,
 input rst,
-input d_rdy,
-input out_akn,
+input in_data_ready,
+input in_akn,
 input [7:0] byte_in,
-output logic d_akn,
-output logic out_rdy,
+output logic out_akn,
+output logic out_data_ready,
 output logic [BPW*8-1:0] word_out
 );
     
     localparam CNT_DW=$clog2(BPW-1);
-    
+    logic prev_data_rdy;
     logic [CNT_DW-1:0]d_cnt;
     logic [BPW-1:0][7:0]cur_word;
     enum logic {DATA,FINISH}state;
+    
+    always_ff @(posedge clk)begin
+        if(rst)prev_data_rdy<=0;
+        else prev_data_rdy<=in_data_ready; 
+    end
+    
     always_ff @(posedge clk)begin
         if(rst)begin
-            d_akn<=0;
+            out_akn<=0;
             word_out<=0;
             cur_word<=0;
             d_cnt<=0;
@@ -41,8 +47,8 @@ output logic [BPW*8-1:0] word_out
         end
         case(state)
             DATA:begin
-                if(d_rdy)begin
-                    d_akn<=1;
+                if(in_data_ready & !prev_data_rdy)begin
+                    out_akn<=1;
                     cur_word[d_cnt]<=byte_in;
                     if(d_cnt==BPW-1)begin
                         d_cnt<=0;
@@ -50,13 +56,13 @@ output logic [BPW*8-1:0] word_out
                     end
                     else d_cnt<=d_cnt+1;
                 end
-                else d_akn<=0;
+                else out_akn<=0;
             end
             FINISH:begin
-                out_rdy<=1;
+                out_data_ready<=1;
                 word_out<=cur_word;
                 state<=DATA;
-                d_akn<=0;
+                out_akn<=0;
             end
         endcase
     end
